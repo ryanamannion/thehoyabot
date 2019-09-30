3826afbe05d3e3e66106b096661bb498e62c714a
Ryan A. Mannion <ryanamannion@users.noreply.github.com>
2019-09-29 22:03:08 -0400

    fixed bugs, added 2019 to 09/29/19 (83pgs)

--- sitescraper.py
+++ sitescraper.py
@@ -1,32 +1,51 @@
-from bs4 import BeautifulSoup
+"""
+sitescraper.py
+
+Ryan A. Mannion
+twitter: @ryanamannion
+github: ryanamannion
+
+This script uses BeautifulSoup to scrape titles from the online archives of thehoya.com,
+Georgetown University's student newspaper. 
+
+The URLs for the archives are built in the following manner: http://www.thehoya.com/[year]/[page number] 
+
+The script cycles through the pages and with the help of HTML picks out and writes the titles to a file.
+
+"""
+
+import os
+import sys
+
 import requests
-from string import digits
+from bs4 import BeautifulSoup
 
 page_counts = {}
 
-page_counts[1998] = 26
-page_counts[1999] = 82
-page_counts[2000] = 101
-page_counts[2001] = 110
-page_counts[2002] = 112
-page_counts[2003] = 118
-page_counts[2004] = 125
-page_counts[2005] = 141
-page_counts[2006] = 146
-page_counts[2007] = 143
-page_counts[2008] = 144
-page_counts[2009] = 140
-page_counts[2010] = 164
-page_counts[2011] = 315
-page_counts[2012] = 190
-page_counts[2013] = 211
-page_counts[2014] = 212
-page_counts[2015] = 218
-page_counts[2016] = 189
-page_counts[2017] = 160
-page_counts[2018] = 131
+# page_counts[1998] = 26
+# page_counts[1999] = 82
+# page_counts[2000] = 101
+# page_counts[2001] = 110
+# page_counts[2002] = 112
+# page_counts[2003] = 118
+# page_counts[2004] = 125
+# page_counts[2005] = 141
+# page_counts[2006] = 146
+# page_counts[2007] = 143
+# page_counts[2008] = 144
+# page_counts[2009] = 140
+# page_counts[2010] = 164
+# page_counts[2011] = 315
+# page_counts[2012] = 190
+# page_counts[2013] = 211
+# page_counts[2014] = 212
+# page_counts[2015] = 218
+# page_counts[2016] = 189
+# page_counts[2017] = 160
+# page_counts[2018] = 131
+page_counts[2019] = 83
 
-file = open("hoyatitles.txt","w")
+file = open(os.path.join(sys.path[0], "hoyatitles.txt"), "w")
 
 count = 0
 
@@ -38,7 +57,7 @@ for year, pages in page_counts.items():
         url = 'http://www.thehoya.com/' + str(year) + '/page/' + str(page) + '/'
 
         source = requests.get(url).text  # read the html source text of page one, feed to BeautifulSoup
-        start_soup = BeautifulSoup(source, 'lxml')
+        start_soup = BeautifulSoup(source, 'html')
 
         for article in start_soup.find_all('div', class_='post-content'):       # skim the first page
 
