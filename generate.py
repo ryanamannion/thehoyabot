aedcc9ed0f6455224e72c18086ca4457641922f4
Ryan A. Mannion <ryanamannion@users.noreply.github.com>
2019-10-06 20:03:43 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -16,38 +16,8 @@ Adapted from code by Amir Zeldes
 from random import choice, random
 from nltk import word_tokenize
 import re
-import pickle
+import pickle, json
 import os, sys
-
-# USE IF YOU DO NOT HAVE A PICKLED DICTIONARY
-# def get_counts(context_length, training_text):
-#     """
-#     This function counts the frequencies of all continuations of each context tuple
-#     :param context_length: Integer, number of tokens preceding current token (use 2 for trigrams)
-#     :param training_text: The training data as one big string
-#     :return: counts: A dictionary of context tuples to dictionaries of continuation probabilities
-#     """
-#
-#     counts = {}
-#
-#     tokens = word_tokenize(training_text)
-#     for i in range(len(tokens) - context_length):   # says how many times we want to move that window in the text
-#         context = []
-#         next_token = tokens[i + context_length]
-#
-#         for j in range(context_length):
-#             context.append(tokens[i + j])
-#
-#         # Add 1 to frequency or create new dictionary item for this tuple
-#         if tuple(context) in counts:
-#             if next_token in counts[tuple(context)]:
-#                 counts[tuple(context)][next_token] += 1     # when next_token is in counts[tuple(context)] uptick by 1
-#             else:
-#                 counts[tuple(context)][next_token] = 1      # otherwise make a new one and set it to 1
-#         else:
-#             counts[tuple(context)] = {next_token: 1}
-#
-#     return counts
 
 
 def load_obj(file):
